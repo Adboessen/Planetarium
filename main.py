1ab1b2c072e2ba8a469e3335a787fb82b13073ee
Adboessen <andrewboessen@amdg.rockhursths.edu>
2020-02-27 09:43:57 -0600

    menu

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 from planetarium import Planetarium
+from planet import Planet
 
 planetarium1 = Planetarium()
 print(str(planetarium1))
+
+planetChoice = input(f'''
+    Choose a Planet:
+    1: Mecury
+    2: Venus
+    3: Earth
+    4: Mars
+    5: Jupiter
+    6: Saturn
+    7: Uranus
+    8: Neptune
+    Choice: ''')
+
+dataChoice = input(f'''
+    Choose Data Type:
+    1: Name
+    2: Diameter
+    3: Distance
+    4: Moons
+    Choice: ''')
+
+
+
+
+
