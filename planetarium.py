0179963840c4bd478aecf9ec7a6409777f29b40c
Adboessen <andrewboessen@amdg.rockhursths.edu>
2020-02-27 09:12:09 -0600

    new

--- planetarium.py
+++ planetarium.py
@@ -1,15 +1,21 @@
 from planet import Planet
 
 class Planetarium(object):
-    
     def __init__(self):
-        self.create
-
+        self.mercury = Planet("Mercury", 3031, 32548000, 0)
+        self.venus = Planet("Venus", 7521, 66871000, 0)
+        self.earth = Planet("Earth", 7917, 92009000, 1)
+        self.mars = Planet("Mars", 4222, 142120000, 2)
+        self.jupiter = Planet("Jupiter", 86881, 484260000, 67)
+        self.saturn = Planet("Saturn", 72367, 930450000, 62)
+        self.uranus = Planet("Uranus", 31518, 1841600000, 27)
+        self.neptune = Planet("Neptune", 30599, 2781900000, 14)
     def __str__(self):
-        return '[' + ', '.join(str(planet) for planet in self.planets) + ']'
-        
-    def create(self):
-        self.planets = []
-        for i in range(0,8):
-            self.planets.append(Planet(i,i,i,i))
-        
+        return f'''{self.mercury}
+{self.venus}
+{self.earth}
+{self.mars}
+{self.jupiter}
+{self.saturn}
+{self.uranus}
+{self.neptune}'''
