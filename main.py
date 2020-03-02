9f43beb8243b7db886cc6791659e15aad42ccec9
Adboessen <andrewboessen@amdg.rockhursths.edu>
2020-03-02 08:12:54 -0600

    user input choice

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
 from planetarium import Planetarium
-from planet import Planet
 
 planetarium1 = Planetarium()
-print(str(planetarium1))
 
-p = {1:"mecury", 2:"venus", 3:"earth", 4:"mars", 5:"jupiter", 6:"saturn", 7:"uranus", 8:"neptune"}
-
-
-planetInput = input(f'''
-    Choose a Planet:
-    1: Mecury
-    2: Venus
-    3: Earth
-    4: Mars
-    5: Jupiter
-    6: Saturn
-    7: Uranus
-    8: Neptune
-    Choice: ''')
-planetChoice = planetInput
-str(planetChoice)
-
-dataInput = input(f'''
-    Choose Data Type:
-    1: Name
-    2: Diameter
-    3: Distance
-    4: Moons
-    Choice: ''')
-dataChoice = dataInput
-int(dataChoice)
-
+def menu():
+    oneOrAll = str(input("Would you like to print one planet or all of them?(1 or All) "))
+    if(oneOrAll == '1'):
+        planetarium1.printOne()
+    elif(oneOrAll == 'All'):
+        print(str(planetarium1))
+        
+choice = 'Y'
+while(choice == 'Y' or choice == 'y'):
+    menu()
+    choice = str(input("Another?(Y or N) "))
 
 
 
